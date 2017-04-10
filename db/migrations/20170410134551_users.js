466a5750ff5b6d434d976b4824389b8ed8f38f6b
Michael Watson <m.watson.tn@gmail.com>
2017-04-10 14:26:01 -0500

    user migration works, pg installed, db folder created

---
+++ db/migrations/20170410134551_users.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+  	table.increments();
+  	table.string('name').notNullable().unique();
+  	table.string('email').notNullable().unique();
+  	table.string('password');
+  	table.string('height');
+  	table.integer('handsize');
+  	table.boolean('citizenship');
+  	table.specificType('likes', knex.raw('text[]'))
+  	table.integer('quest1');
+  	table.integer('quest2');
+  	table.integer('quest3');
+  	table.integer('quest4');
+  	table.integer('quest5');
+  	table.integer('score');
+
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users')
+};
