4021fbcf8298750c53e3e3e2207cd9dee504c4c5
T-Farmer <terrillfarmer@gmail.com>
2017-04-11 11:38:45 -0500

    public and private routes built

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const {Router} = require('express');
+const router = Router();
+
+//public routes
+router.use(require('./login-register'))
+
+//login guard middleware that reroutes home if not registered
+router.use((req, res, next) => {
+  if(req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect('/index')
+  }
+});
+
+//private routes
+router.use(require('./logout'))
+//in case myLikes route is actually needed
+router.use(require('./myLikes'))
+
+module.exports = router;
