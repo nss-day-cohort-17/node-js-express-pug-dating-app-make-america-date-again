54e5d66e88c5a197b960283f36bf2e7fe06773be
Ben Conner <ben.conner40@gmail.com>
2017-04-12 15:55:42 -0500

    users file and delete second response file

--- models/users.js
+++ models/users.js
@@ -1,7 +1,33 @@
-'use strict';
+// 'use strict';
+//
+// const {bookshelf} = require('./db/database');
+// const User = bookshelf.Model.extend({
+//   tableName: 'users'
+// });
+// module.exports = User;
+'use strict'
 
-const {bookshelf} = require('./db/database');
+const { bookshelf } = require('../db/database');
+
+const { compare } = require('bcryptjs')
 const User = bookshelf.Model.extend({
-  tableName: 'users'
-});
-module.exports = User;
+  tableName: 'users',
+  bcrypt: {field: 'password'},
+  comparePass: function(passwordStr){
+    return compare(passwordStr, this.attributes.password)
+  }
+}, {
+    findOneByEmail: function(email){
+      return this.forge ({email})
+      .fetch()
+      .then( (user) => {
+        console.log("findOneByEmail user model", user.get('email'));
+        return user;
+      })
+      .catch( () => {
+        console.log('findOneByEmail catch user model');
+        return null
+      })
+    }
+})
+module.exports.User = User;
